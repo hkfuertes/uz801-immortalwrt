44de1b6454ed84cebe4ebb2b324d8997bf120ffb
wyref <ferywu@gmail.com>
2025-08-12 17:49:55 +0700

    enable bt, modem
    add some repo too

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,25 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+#sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+cat <<EOF >> target/linux/msm89xx/config-6.1
+CONFIG_BT_QCOMSMD=m
+CONFIG_QCOM_IPA=m
+EOF
+
+cat <<EOF >> target/linux/msm89xx/config-6.6
+CONFIG_BT_QCOMSMD=m
+CONFIG_QCOM_IPA=m
+EOF
+
+cat <<EOF >> target/linux/msm89xx/config-6.12
+CONFIG_BT_QCOMSMD=m
+CONFIG_QCOM_IPA=m
+EOF
+
+# HelmiWrt packages
+git clone --depth=1 https://github.com/Haris131/helmiwrt-packages package/community
